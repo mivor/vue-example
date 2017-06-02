93cfe7b6f6166351c4aeda386edfab3147453d06
Ferenc Grecu <ferenc.grecu@accesa.eu>
2017-06-02 14:26:39 +0300

    Change App to be a Bear editor

--- app/src/views/App.vue
+++ app/src/views/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
-    <img src="../assets/logo.png">
-    <router-view></router-view>
+    <h1>{{title}}</h1>
+    <h2>{{bear.name}} details</h2>
+    <div><label>id: </label>{{bear.id}}</div>
+    <div>
+      <label>name: </label>
+      <input v-model="bear.name" placeholder="name">
+      </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      title: 'Bears are Awesome',
+      bear: {
+        id: 1,
+        name: 'Ursus',
+      },
+    };
+  },
 };
 </script>
 
