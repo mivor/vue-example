5c92e8a7adc5dd4bbd669d1097affd34cdcccbc1
Ferenc Grecu <ferenc.grecu@accesa.eu>
2017-06-02 16:21:39 +0300

    Add Dashboad view and router

--- app/src/configs/router-config.js
+++ app/src/configs/router-config.js
@@ -1,8 +1,13 @@
 import Bears from '@/views/Bears';
+import Dashboard from '@/views/Dashboard';
 
 export default [
   {
     path: '/bears',
     component: Bears,
+  },
+  {
+    path: '/dashboard',
+    component: Dashboard,
   },
 ];
---
+++ app/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>My Dashboard</h3>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
