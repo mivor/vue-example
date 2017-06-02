7a3f0759bb95fb1cb8d8ece31e0d1a177100aa08
Ferenc Grecu <ferenc.grecu@accesa.eu>
2017-06-02 17:45:52 +0300

    Add navigation styles

--- app/src/views/App.vue
+++ app/src/views/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <h1>{{title}}</h1>
     <nav>
-      <router-link to="/dashboard">Dashboard</router-link>
-      <router-link to="/bears">Bears</router-link>
+      <router-link activeClass="active" to="/dashboard">Dashboard</router-link>
+      <router-link activeClass="active" to="/bears">Bears</router-link>
     </nav>
     <router-view />
   </div>
@@ -19,3 +19,34 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 1.2em;
+  color: #999;
+  margin-bottom: 0;
+}
+h2 {
+  font-size: 2em;
+  margin-top: 0;
+  padding-top: 0;
+}
+nav a {
+  padding: 5px 10px;
+  text-decoration: none;
+  margin-top: 10px;
+  display: inline-block;
+  background-color: #eee;
+  border-radius: 4px;
+}
+nav a:visited, a:link {
+  color: #607D8B;
+}
+nav a:hover {
+  color: #039be5;
+  background-color: #CFD8DC;
+}
+nav a.active {
+  color: #039be5;
+}
+</style>
