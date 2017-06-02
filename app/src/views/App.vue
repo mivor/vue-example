b42c00f1c0110f4dca5bd1a92c2612d92be05c52
Ferenc Grecu <ferenc.grecu@accesa.eu>
2017-06-02 14:49:37 +0300

    Hide bear details when no bear is selected

--- app/src/views/App.vue
+++ app/src/views/App.vue
@@ -7,12 +7,15 @@
         <span class="badge">{{bear.id}}</span> {{bear.name}}
       </li>
     </ul>
-    <h2>{{selectedBear.name}} details</h2>
-    <div><label>id: </label>{{selectedBear.id}}</div>
-    <div>
-      <label>name: </label>
-      <input v-model="selectedBear.name" placeholder="name">
+    <div v-if="selectedBear">
+      <h2>{{selectedBear.name}} details</h2>
+      <div>
+        <label>id: </label>{{selectedBear.id}}</div>
+      <div>
+        <label>name: </label>
+        <input v-model="selectedBear.name" placeholder="name">
       </div>
+    </div>
   </div>
 </template>
 
@@ -36,10 +39,7 @@ export default {
     return {
       title: 'Bears are Awesome',
       bears: testBears,
-      selectedBear: {
-        id: 1,
-        name: 'Ursus',
-      },
+      selectedBear: null,
     };
   },
   methods: {
@@ -52,50 +52,56 @@ export default {
 
 <style>
 .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .bears {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .bears li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .bears li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .bears li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .bears .text {
-    position: relative;
-    top: -3px;
-  }
-  .bears .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
+  background-color: #CFD8DC !important;
+  color: white;
+}
+
+.bears {
+  margin: 0 0 2em 0;
+  list-style-type: none;
+  padding: 0;
+  width: 15em;
+}
+
+.bears li {
+  cursor: pointer;
+  position: relative;
+  left: 0;
+  background-color: #EEE;
+  margin: .5em;
+  padding: .3em 0;
+  height: 1.6em;
+  border-radius: 4px;
+}
+
+.bears li.selected:hover {
+  background-color: #BBD8DC !important;
+  color: white;
+}
+
+.bears li:hover {
+  color: #607D8B;
+  background-color: #DDD;
+  left: .1em;
+}
+
+.bears .text {
+  position: relative;
+  top: -3px;
+}
+
+.bears .badge {
+  display: inline-block;
+  font-size: small;
+  color: white;
+  padding: 0.8em 0.7em 0 0.7em;
+  background-color: #607D8B;
+  line-height: 1em;
+  position: relative;
+  left: -1px;
+  top: -4px;
+  height: 1.8em;
+  margin-right: .8em;
+  border-radius: 4px 0 0 4px;
+}
 </style>
