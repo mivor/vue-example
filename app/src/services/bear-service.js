ecb5e28fb55983c4e21e50cd06399e76459bd445
Ferenc Grecu <ferenc.grecu@accesa.eu>
2017-06-02 20:59:04 +0300

    Extend bear service with API methods

--- app/src/services/bear-service.js
+++ app/src/services/bear-service.js
@@ -1,13 +1,29 @@
 import axios from 'axios';
 
-const http = axios.create({
+const server = axios.create({
   baseURL: 'http://localhost:3000/api/',
   timeout: 1000,
 });
 
 export default {
-  async getBears() {
-    const res = await http.get('bears');
+  async getBears(count) {
+    const res = await server.get('/bears', { params: { count } });
     return res.data;
+  },
+  async getBear(id) {
+    const res = await server.get(`/bear/${id}`);
+    return res.data;
+  },
+  async create(bear) {
+    const res = await server.post('/bears', bear);
+    const id = res.data.id;
+    bear.id = id;
+    return bear;
+  },
+  update(bear) {
+    return server.put(`/bear/${bear.id}`, bear);
+  },
+  delete(id) {
+    return server.delete(`/bear/${id}`);
   },
 };
