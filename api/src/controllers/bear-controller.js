bf1f4b2cab453b0d51fca899ffa781953a5b4bfe
Ferenc Grecu <ferenc.grecu@accesa.eu>
2017-06-02 18:59:06 +0300

    Implement bear API

--- api/src/controllers/bear-controller.js
+++ api/src/controllers/bear-controller.js
@@ -11,16 +11,45 @@ const mockBears = [
   { id: 20, name: 'Telutci' },
 ];
 
+function findBear(id) {
+  const bearId = parseInt(id, 10);
+  return mockBears.find(x => x.id === bearId);
+}
+
 export default function (api) {
-  api.get('/bears', (req, res) => {
-    const count = parseInt(req.query.count, 10);
+  api.route('/bears')
+    .get((req, res) => {
+      const count = parseInt(req.query.count, 10);
 
-    if (count) res.json(mockBears.slice(0, count));
-    else res.json(mockBears);
-  });
-  api.get('/bear/:id', (req, res) => {
-    const bearId = parseInt(req.params.id, 10);
+      if (count) res.json(mockBears.slice(0, count));
+      else res.json(mockBears);
+    })
+    .post((req, res) => {
+      const lastBear = mockBears[mockBears.length - 1];
+      const newBear = {
+        id: lastBear.id + 1,
+        name: req.body.name,
+      };
+      mockBears.push(newBear);
 
-    res.json(mockBears.find(x => x.id === bearId));
-  });
+      res.json({ id: newBear.id });
+    });
+
+  api.route('/bear/:id')
+    .get((req, res) => {
+      res.json(findBear(req.params.id));
+    })
+    .put((req, res) => {
+      const bear = findBear(req.params.id);
+      bear.name = req.body.name;
+
+      res.sendStatus(200);
+    })
+    .delete((req, res) => {
+      const bear = findBear(req.params.id);
+      const index = mockBears.indexOf(bear);
+      mockBears.splice(index, 1);
+
+      res.sendStatus(200);
+    });
 }
