197754a1032f9152f04733a9cf1e5031791d9009
Ferenc Grecu <ferenc.grecu@accesa.eu>
2017-06-02 18:27:12 +0300

    Add bear controller

---
+++ api/src/controllers/bear-controller.js
@@ -0,0 +1,22 @@
+const mockBears = [
+  { id: 11, name: 'Jammy' },
+  { id: 12, name: 'Charlie' },
+  { id: 13, name: 'Hohots' },
+  { id: 14, name: 'Henri' },
+  { id: 15, name: 'Art' },
+  { id: 16, name: 'Coco' },
+  { id: 17, name: 'Edun' },
+  { id: 18, name: 'Kuruk' },
+  { id: 19, name: 'Bemot' },
+  { id: 20, name: 'Telutci' },
+];
+
+export default function (api) {
+  api.get('/bears', (req, res) => {
+    res.json(mockBears);
+  });
+  api.get('/bear/:id', (req, res) => {
+    const bearId = parseInt(req.params.id, 10);
+    res.json(mockBears.find(x => x.id === bearId));
+  });
+}
--- api/src/main.js
+++ api/src/main.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import registerBearController from './controllers/bear-controller';
+
 const app = express();
 const port = process.env.PORT || 3000;
+const router = express.Router();
+
+registerBearController(router);
+
+router.get('/', (req, res) => res.json({ message: 'Bear API running!' }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'hey! welcome to our api!' });
-});
-
 app.use('/api', router);
-
 app.listen(port);
 console.log(`Magic happens on port ${port}`);
