53d7ddd5c824e4b9ccd7dfac9aab528f20337b22
Ferenc Grecu <ferenc.grecu@accesa.eu>
2017-06-02 20:45:51 +0300

    Add error handling

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -3,14 +3,24 @@ import BearService from '../services/bear-service';
 export const INITIALIZE = 'INITIALIZE';
 export const INITIALIZED = 'INITIALIZED';
 
+function handleErrors(error) {
+  // TODO: error handling
+  // eslint-disable-next-line no-console
+  console.error(error);
+}
+
 export default {
   state: {
     bears: [],
   },
   actions: {
     async [INITIALIZE]({ commit }) {
-      const bears = await BearService.getBears();
-      commit(INITIALIZED, { bears });
+      try {
+        const bears = await BearService.getBears();
+        commit(INITIALIZED, { bears });
+      } catch (error) {
+        handleErrors(error);
+      }
     },
   },
   mutations: {
